ffa34c5f390fb0ce85454e30561b909856fe60e7
MichelCode <michelseocode.com>
2023-11-22 16:43:57 +0100

    vueboostV4 Navbar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import ButtonComponent from "../components/ButtonComponent.vue";
+import TitleComponents from "../components/TitleComponent.vue";
 </script>
 
 <template>
   <main>
+    <TitleComponents>Mes projets</TitleComponents>
     <ButtonComponent />
   </main>
 </template>
